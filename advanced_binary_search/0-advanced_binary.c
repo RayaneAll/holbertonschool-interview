3138a4de344f8349674ddb587e96f8023bc57f5e
RayaneAll <9975@holbertonstudents.com>
2026-06-05 14:54:16 +0200

    Refactor advanced binary search implementation with helper function and array printing

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,42 +1,83 @@
 #include <stdio.h>
 #include "search_algos.h"
 
-static int binary_search(int *array, int left, int right, int value);
-
-int advanced_binary(int *array, size_t size, int value)
+/**
+ * print_array - Affiche un sous-tableau d'entiers
+ * @array: Pointeur sur le tableau
+ * @left: Index gauche du sous-tableau
+ * @right: Index droit du sous-tableau
+ */
+void print_array(int *array, size_t left, size_t right)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+	size_t i;
 
-    return (binary_search(array, 0, (int)size - 1, value));
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
 }
 
-static int binary_search(int *array, int left, int right, int value)
+/**
+ * advanced_binary_helper - Recherche binaire récursive
+ *                           qui retourne la première occurrence
+ * @array: Tableau trié d'entiers
+ * @left: Bord gauche du sous-tableau courant
+ * @right: Bord droit du sous-tableau courant
+ * @value: Valeur à chercher
+ *
+ * Return: Index de la première occurrence, ou -1
+ */
+int advanced_binary_helper(int *array, size_t left, size_t right, int value)
 {
-    int mid, i;
+	size_t mid;
 
-    if (left > right)
-        return (-1);
+	if (left > right)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
+	print_array(array, left, right);
 
-    mid = left + (right - left) / 2;
-    if (array[mid] == value)
-    {
-        int idx = binary_search(array, left, mid - 1, value);
+	mid = left + (right - left) / 2;
 
-        if (idx != -1)
-            return (idx);
-        return (mid);
-    }
-    if (array[mid] < value)
-        return (binary_search(array, mid + 1, right, value));
-    return (binary_search(array, left, mid - 1, value));
+	if (array[mid] == value)
+	{
+		/* Vérifier si c'est la première occurrence dans ce segment */
+		if (mid == left || array[mid - 1] != value)
+			return ((int)mid);
+
+		/* Sinon, il y a encore value à gauche : on garde mid dans la fenêtre */
+		return (advanced_binary_helper(array, left, mid, value));
+	}
+	else if (array[mid] < value)
+	{
+		/* Chercher dans la moitié droite (mid exclu, car trop petit) */
+		return (advanced_binary_helper(array, mid + 1, right, value));
+	}
+	else /* array[mid] > value */
+	{
+		/*
+		 * Chercher dans moitié gauche en gardant mid (trop grand) dans la fenêtre
+		 * pour obtenir des affichages comme "11, 21, 31, 33" puis "31, 33".
+		 */
+		return (advanced_binary_helper(array, left, mid, value));
+	}
+}
+
+/**
+ * advanced_binary - Recherche avancée dans un tableau trié
+ * @array: Pointeur sur le premier élément du tableau
+ * @size: Nombre d'éléments du tableau
+ * @value: Valeur à chercher
+ *
+ * Return: Index de la première occurrence, ou -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_helper(array, 0, size - 1, value));
 }
